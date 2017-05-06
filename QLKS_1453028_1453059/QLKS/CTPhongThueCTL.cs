8c528493c6e98b993f7f5b5be9309098f31c5880
BeoU <nttramy2103@gmail.com>
2017-05-07 04:10:14 +0700

    Update chi tiet thue phong

---
+++ QLKS_1453028_1453059/QLKS/CTPhongThueCTL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Data;
+
+namespace QLKS
+{
+    class CTPhongThueCTL
+    {
+        private CTPhongThueDAO dataAccess = new CTPhongThueDAO();
+        public DataTable getTableCTPhongThue()
+        {
+            return dataAccess.getTableCTPhongThue();
+        }
+
+        public DataTable searchDate(DateTime date)
+        {
+            return dataAccess.searchDate(date);
+        }
+
+        public DataTable searchTenOrCMND(string str)
+        {
+            return dataAccess.searchTenOrCMND(str);
+        }
+
+        public void insert(CTPhongThueDTO info)
+        {
+            dataAccess.insert(info);
+        }
+
+        public void update(CTPhongThueDTO info)
+        {
+            dataAccess.update(info);
+        }
+    }
+}
---
+++ QLKS_1453028_1453059/QLKS/ThemPhieuThueInterface.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace QLKS
+{
+    partial class ThemPhieuThueInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTienDatCoc = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHoTen = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCMND = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtGia = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(171, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PHIẾU THUÊ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Họ tên:";
+            // 
+            // txtTienDatCoc
+            // 
+            this.txtTienDatCoc.Location = new System.Drawing.Point(176, 294);
+            this.txtTienDatCoc.Name = "txtTienDatCoc";
+            this.txtTienDatCoc.Size = new System.Drawing.Size(234, 22);
+            this.txtTienDatCoc.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 297);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Tiền đặt cọc:";
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.AutoSize = true;
+            this.txtHoTen.Location = new System.Drawing.Point(117, 70);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(0, 17);
+            this.txtHoTen.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "CMND:";
+            // 
+            // txtCMND
+            // 
+            this.txtCMND.AutoSize = true;
+            this.txtCMND.Location = new System.Drawing.Point(117, 113);
+            this.txtCMND.Name = "txtCMND";
+            this.txtCMND.Size = new System.Drawing.Size(0, 17);
+            this.txtCMND.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Mã phòng:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(47, 251);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Giờ trả phòng:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(47, 203);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 17);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Ngày trả phòng:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(47, 341);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 17);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Giá:";
+            // 
+            // txtGia
+            // 
+            this.txtGia.AutoSize = true;
+            this.txtGia.Location = new System.Drawing.Point(176, 341);
+            this.txtGia.Name = "txtGia";
+            this.txtGia.Size = new System.Drawing.Size(0, 17);
+            this.txtGia.TabIndex = 13;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(176, 203);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(234, 22);
+            this.dateTimePicker1.TabIndex = 14;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker2.Location = new System.Drawing.Point(176, 251);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(234, 22);
+            this.dateTimePicker2.TabIndex = 15;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(86, 386);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 32);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Thêm khách hàng";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(278, 386);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 32);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Lập phiếu";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // ThemPhieuThueInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 463);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtGia);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCMND);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.txtTienDatCoc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ThemPhieuThueInterface";
+            this.Text = "ThemPhieuThueInterface";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTienDatCoc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label txtHoTen;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label txtCMND;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label txtGia;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
